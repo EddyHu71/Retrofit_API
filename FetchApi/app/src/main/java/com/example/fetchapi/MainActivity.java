9cb1ac99f9e3e546dee054d5d45cb13096a950ea
Eddy <eddyhu71@gmail.com>
2021-04-29 22:02:09 +0700

    fix fetch listview

--- FetchApi/app/src/main/java/com/example/fetchapi/MainActivity.java
+++ FetchApi/app/src/main/java/com/example/fetchapi/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -16,11 +17,16 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     ListView superList;
+    ArrayAdapter<String> myAdapter;
+    List<String> myHeroes = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         superList = (ListView) findViewById(R.id.superListView);
+        myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myHeroes);
+        superList.setAdapter(myAdapter);
         getSuperHeroes();
     }
 
@@ -31,15 +37,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<List<Results>> call, Response<List<Results>> response) {
                 List<Results> myheroList = response.body();
-                String[] oneHeroes = new String[myheroList.size()];
+//                String[] oneHeroes = new String[myheroList.size()];
                 Log.d("TAG", "Success");
 //                Log.d("TAG", myheroList.toString());
                 Toast.makeText(getApplicationContext(), "Successfully fetched", Toast.LENGTH_LONG).show();
                 for (int i =0;i<myheroList.size();i++) {
-                    oneHeroes[i] = myheroList.get(i).getName();
-                    Log.d("TAG", oneHeroes[i]);
+//                    oneHeroes[i] = myheroList.get(i).getName();
+//                    Log.d("TAG", oneHeroes[i]);
+                        myHeroes.add(myheroList.get(i).getName());
+
                 }
-                superList.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, oneHeroes));
+                myAdapter.notifyDataSetChanged();
+//                superList.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, oneHeroes));
             }
 
             @Override
